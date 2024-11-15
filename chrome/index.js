fba763392f1edcbb0229dd5eaeafb4d0c050ff94
Eric Chamberlain <eric.j.chamberlain@protonmail.com>
2024-11-15 09:33:22 -0800

    Only close menus once when selecting option Simplified how os functions are executed

--- chrome/index.js
+++ chrome/index.js
@@ -198,7 +198,6 @@ function stylePopupMenus() {
                         break;
                     }
                 }
-                closeAllMenus();
             });
             choices.appendChild(choice);
         }
